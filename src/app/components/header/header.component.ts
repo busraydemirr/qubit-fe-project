fc2a156201caf1ec6828e32c4dd63d2ce72f99a1
busra aydemir <busranur.aydemir@ogr.iu.edu.tr>
2025-01-11 02:01:31 +0300

    add header component

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,15 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-header',
+  imports: [MatToolbarModule, MatIconModule, MatButtonModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss',
+})
+export class HeaderComponent {
+  @Input() public title: string = '';
+  @Output() public openMenu: EventEmitter<void> = new EventEmitter<void>();
+}
