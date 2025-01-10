45a76312b8bab0bd0c37f45f1af93f5bf9f61d72
busra aydemir <busranur.aydemir@ogr.iu.edu.tr>
2025-01-11 02:00:24 +0300

    add tailwind and angular material

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  important: true,
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      inter: ["Inter", "sans-serif"],
+    },
+  },
+  plugins: [],
+};
