54c5f1df27daf11ad1b167567e3539cb5350c719
busra aydemir <busranur.aydemir@ogr.iu.edu.tr>
2025-01-11 02:05:03 +0300

    add routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './features/auth/login/login.component';
+import { HomepageComponent } from './features/homepage/homepage.component';
+import { CashTransactionsComponent } from './features/cash-transactions/cash-transactions.component';
+import { CurrentAccountsComponent } from './features/current-accounts/current-accounts.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'auth/login', pathMatch: 'full' },
+  { path: 'auth/login', component: LoginComponent },
+  {
+    path: 'home',
+    component: HomepageComponent,
+    children: [
+      { path: '', redirectTo: 'cash-transactions', pathMatch: 'full' },
+      { path: 'cash-transactions', component: CashTransactionsComponent },
+      { path: 'current-accounts', component: CurrentAccountsComponent },
+    ],
+  },
+  { path: '**', redirectTo: 'auth/login' },
+];
