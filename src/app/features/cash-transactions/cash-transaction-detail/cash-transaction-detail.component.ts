15f6f02c6d77a93239bcc1256fc99f82fd8d706e
busra aydemir <busranur.aydemir@ogr.iu.edu.tr>
2025-02-08 00:58:01 +0300

    add cash detail tab and filter fields

--- src/app/features/cash-transactions/cash-transaction-detail/cash-transaction-detail.component.ts
+++ src/app/features/cash-transactions/cash-transaction-detail/cash-transaction-detail.component.ts
@@ -2,15 +2,34 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { BannerComponent } from '../../../components/banner/banner.component';
 import { ActivatedRoute, Params } from '@angular/router';
 import { SubSink } from 'subsink';
-
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 @Component({
   selector: 'app-cash-transaction-detail',
-  imports: [BannerComponent],
+  imports: [
+    BannerComponent,
+    MatTabsModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
   templateUrl: './cash-transaction-detail.component.html',
   styleUrl: './cash-transaction-detail.component.scss',
 })
 export class CashTransactionDetailComponent implements OnInit, OnDestroy {
   public cashName: string = '';
+  public cashFilterForm!: FormGroup;
   private _subSink: SubSink = new SubSink();
 
   constructor(private route: ActivatedRoute) {}
@@ -19,8 +38,20 @@ export class CashTransactionDetailComponent implements OnInit, OnDestroy {
     this._subSink.sink = this.route.params.subscribe((params: Params) => {
       const cashId = params['id'];
 
-      // TODO: istek atılıp name setlenmeli 
+      // TODO: istek atılıp name setlenmeli
       this.cashName = `Idsi ${cashId} olan kasa ismi`;
+    });
+
+    this._buildForm();
+  }
+
+  public filter(): void {}
+
+  private _buildForm(): void {
+    this.cashFilterForm = new FormGroup({
+      name: new FormControl(''),
+      phoneNumber: new FormControl(''),
+      email: new FormControl('', [Validators.email]),
     });
   }
 
